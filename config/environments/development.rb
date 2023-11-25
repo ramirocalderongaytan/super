5e301bcf1592f85db28d46a7d91b5c86050e638a
Ramiro Calderon <ramirocalderongaytan@gmail.com>
2023-11-25 17:22:43 -0600

    gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
-
+  config.hosts << "rails-production-2daa.up.railway.app"
   # Enable server timing
   config.server_timing = true
 
