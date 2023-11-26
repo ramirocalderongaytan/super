51af9cff9fb6a3216cbb2dbb92d40125ab8b20cb
Ramiro Calderon <ramirocalderongaytan@gmail.com>
2023-11-25 18:50:04 -0600

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -14,4 +14,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_26_004029) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "depar
+  create_table "departments", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
