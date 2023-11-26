9f862fd143e7d69ca26248cd2eab489c19e26012
Ramiro Calderon <ramirocalderongaytan@gmail.com>
2023-11-25 18:49:16 -0600

    new root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "departments#index"
+  
   resources :departments
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -14,10 +14,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_26_004029) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "departments", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-end
+  create_table "depar
