37fc74c860d223a9672011be06edeb06673940e6
Ramiro Calderon <ramirocalderongaytan@gmail.com>
2023-11-25 18:43:03 -0600

    create departments db

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  root "departments#index"
+  resources :departments
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
 
   # Defines the root path route ("/")
   # root "articles#index"
 end
+
